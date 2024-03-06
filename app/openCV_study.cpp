fd09c0cbb7bbd6aa99496c6153518acd79f633c6
hanyouyang <hanyouyang@zoezen.cn>
2024-03-06 09:32:13 +0800

    增加首页

--- app/openCV_study.cpp
+++ app/openCV_study.cpp
@@ -1,10 +1,34 @@
 #include "openCV_study.h"
 
 openCV_study::openCV_study(QWidget *parent)
-    : QWidget(parent)
+    : QWidget(parent),
+    ui(new Ui::openCV_studyClass)
 {
-    ui.setupUi(this);
+    ui->setupUi(this);
+
+    //初始化窗口
+    InitWindow();
+    //初始化信号和槽
+    InitConnections();
 }
 
 openCV_study::~openCV_study()
 {}
+
+void openCV_study::InitWindow()
+{
+  QScreen *primaryScreen = QGuiApplication::primaryScreen();
+  QRect primaryScreenGeometry = primaryScreen->geometry();
+
+  this->setWindowFlags(Qt::Window | Qt::WindowMinimizeButtonHint | Qt::WindowCloseButtonHint);
+
+  this->move(primaryScreenGeometry.x(), primaryScreenGeometry.y());
+}
+
+void openCV_study::InitConnections()
+{
+  //页面切换
+  connect(ui->comboBox, QOverload<int>::of(&QComboBox::currentIndexChanged), this, [=] (int index) {
+    ui->stackedWidget->setCurrentIndex(index);
+  });
+}
--- app/openCV_study.h
+++ app/openCV_study.h
@@ -1,7 +1,13 @@
 #pragma once
 
 #include <QtWidgets/QWidget>
+#include <QScreen>
+
 #include "ui_openCV_study.h"
+
+namespace Ui{
+  class openCV_studyClass;
+}
 
 class openCV_study : public QWidget
 {
@@ -11,6 +17,12 @@ public:
     openCV_study(QWidget *parent = nullptr);
     ~openCV_study();
 
+    //初始化出口
+    void InitWindow();
+
+    //初始化信号和槽
+    void InitConnections();
+
 private:
-    Ui::openCV_studyClass ui;
+    Ui::openCV_studyClass *ui;
 };
